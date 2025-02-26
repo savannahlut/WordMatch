5b3b6dbf19c495c872ebcc3b865ad7de45107825
Savannah-Lu <savannahlut@nycstudents.net>
2025-02-26 20:34:38 +0000

    starting

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args)
+    {
+        WordMatch game = new WordMatch("mississippi");
+        System.out.println(game.scoreGuess("i"));
+        System.out.println(game.scoreGuess("iss"));
+        System.out.println(game.scoreGuess("issipp"));
+    }
+}
